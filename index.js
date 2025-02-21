e8a54f75111dc1a733c0d6ab4eefbfd35b8c803e
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-21 19:08:50 +0700

    fix index.js because prettier formatter

--- index.js
+++ index.js
@@ -4,51 +4,52 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul() {
+function tasksIrul(){
     // Program: Sum Number
     // Success
-    const resultSum = irul.sumNumber(10, 50);
+    const resultSum = irul.sumNumber(10,50);
     console.log(resultSum);
 
     // Failed
-    try {
-        const result = irul.sumNumber("10", 30);
+    try{
+        const result = irul.sumNumber("10",30);
         console.log(result);
-    } catch (error) {
+    }catch(error){
         console.log(error.message);
     }
     // Program: Multiply Number
     // Success
-    const resultMultiply = irul.multiplyNumbers(10, 50);
+    const resultMultiply = irul.multiplyNumbers(10,50);
     console.log(resultMultiply);
 
     // Failed
-    try {
-        const result = irul.multiplyNumbers("10", 30);
+    try{
+        const result = irul.multiplyNumbers("10",30);
         console.log(result);
-    } catch (error) {
+    }catch(error){
         console.log(error.message);
     }
     // Program: Convert Celcius to Fahrenheit
     // Success
     const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(
-        `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
-    );
+    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
 
     // Failed
-    try {
-        const result = irul.celciusToFahrenheit("10", 30);
+    try{
+        const result = irul.celciusToFahrenheit("10",30);
         console.log(result);
-    } catch (error) {
+    }catch(error){
         console.log(error.message);
     }
 }
 tasksIrul();
 
 /**
- * Task "mhakimsaputra"
+ * Task Hakim
  */
+
+// import your tasks here
+
 const hakim = require("./tasks/mhakimsaputra");
 function tasksHakim() {
     hakim.triangle(5);
@@ -60,6 +61,3 @@ function tasksHakim() {
 }
 
 tasksHakim();
-
-// import your tasks here
-// const hakim = require("./tasks/mhakimsaputra");
