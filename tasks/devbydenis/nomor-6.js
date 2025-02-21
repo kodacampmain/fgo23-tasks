f9ee25d8787c2a9ec72a82712217184f5563ce7b
devbydenis <hello.denisrahmadi@gmail.com>
2025-02-21 18:20:14 +0700

    feat: nomor-6.js

---
+++ tasks/devbydenis/nomor-6.js
@@ -0,0 +1,14 @@
+const buatRentang = (num1, num2) => {
+  let result = []
+
+  for (let i = num1; i <= num2; i++) {
+    result.push(i)
+  }
+
+  return result.join(", ")
+}
+
+// console.log(buatRentang(1, 5));
+// console.log(buatRentang(5, 10));
+
+module.exports = buatRentang
