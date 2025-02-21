184bd3b47b7037ab3cb0f3929a480461f0635090
devbydenis <hello.denisrahmadi@gmail.com>
2025-02-21 19:21:58 +0700

    add: nomor-3.js to calculate triangle reverse

--- tasks/devbydenis/nomor-3.js
+++ tasks/devbydenis/nomor-3.js
@@ -7,7 +7,7 @@ const triangle = (number) => {
     for (let i = 1; i <= number; i++) {
         let row = ""; 
 
-        for (let j = 1; j <= i; j++) {
+        for (let j = number; j >= i; j--) {
             row += j + " ";
         }
 
@@ -21,6 +21,6 @@ const triangle = (number) => {
 // triangle(-1)
 // console.log(triangle(1));
 // console.log(triangle(2));
-// console.log(triangle(5));
+console.log(triangle(5));
 
 module.exports = triangle;
