4011ebe8babd4c22fb0fbcddbdc5d58751f09a4a
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-21 18:06:17 +0700

    feat: add function triangle3

---
+++ tasks/mhakimsaputra/number3.js
@@ -0,0 +1,19 @@
+const triangle3 = (num) => {
+  if (typeof num !== "number" || num <= 0) {
+    return console.log(
+      "Parameter harus bertipe data nomor dan harus nomor positif"
+    );
+  }
+  for (let i = 1; i <= num; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += j;
+    }
+    let arr = [...row];
+    arr.reverse();
+    arr = arr.join("");
+    console.log(arr);
+  }
+};
+
+module.exports = triangle3;
