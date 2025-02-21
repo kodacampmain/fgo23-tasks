9a96928a119aedd2187f19dad256f91a6ebcc3e4
Rieza Eka Tomara <riezaekatomara@gmail.com>
2025-02-21 18:54:22 +0700

    index2

--- index.js
+++ index.js
@@ -4,48 +4,111 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul(){
-    // Program: Sum Number
-    // Success
-    const resultSum = irul.sumNumber(10,50);
-    console.log(resultSum);
+function tasksIrul() {
+  // Program: Sum Number
+  // Success
+  const resultSum = irul.sumNumber(10, 50);
+  console.log(resultSum);
 
-    // Failed
-    try{
-        const result = irul.sumNumber("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Multiply Number
-    // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
-    console.log(resultMultiply);
+  // Failed
+  try {
+    const result = irul.sumNumber("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Multiply Number
+  // Success
+  const resultMultiply = irul.multiplyNumbers(10, 50);
+  console.log(resultMultiply);
 
-    // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Convert Celcius to Fahrenheit
-    // Success
-    const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
+  // Failed
+  try {
+    const result = irul.multiplyNumbers("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Convert Celcius to Fahrenheit
+  // Success
+  const temperatureResult = irul.celciusToFahrenheit(10);
+  console.log(
+    `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+  );
 
-    // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
+  // Failed
+  try {
+    const result = irul.celciusToFahrenheit("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 tasksIrul();
 
 /**
- * Task "Person Name"
+ * Task
+ */
+
+/**
+ * Task Irul
+ */
+
+const irul = require("./tasks/irul");
+
+function tasksIrul() {
+  // Program: Sum Number
+  // Success
+  const resultSum = irul.sumNumber(10, 50);
+  console.log(resultSum);
+
+  // Failed
+  try {
+    const result = irul.sumNumber("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Multiply Number
+  // Success
+  const resultMultiply = irul.multiplyNumbers(10, 50);
+  console.log(resultMultiply);
+
+  // Failed
+  try {
+    const result = irul.multiplyNumbers("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Convert Celcius to Fahrenheit
+  // Success
+  const temperatureResult = irul.celciusToFahrenheit(10);
+  console.log(
+    `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+  );
+
+  // Failed
+  try {
+    const result = irul.celciusToFahrenheit("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+tasksIrul();
+
+/**
+ * Task Rieza Eka Tomara
  */
 
 // import your tasks here
+
+const {
+  triangleAs,
+  countArray,
+  triangleDes,
+  processNumber,
+  cetakTabel,
+  buatRentang,
+} = require("./tasks/fgorieza/index.js");
