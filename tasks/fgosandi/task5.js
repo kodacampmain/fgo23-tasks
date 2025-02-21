716146965ed6f7ca2669408f74aa3c460da7ba3b
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-02-21 17:34:16 +0700

    add function cetak tebal

---
+++ tasks/fgosandi/task5.js
@@ -0,0 +1,11 @@
+const cetakTebal = (n) => {
+  if (typeof n !== 'number' || n < 1) {
+    console.log('Parameter harus bertipe data nomor dan harus nomor positif')
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${n} X ${i} = ${n * i}`)
+  }
+}
+
+cetakTebal(3)
