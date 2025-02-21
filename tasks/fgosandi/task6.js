c4bc2b1f1a9c99dd160e5681eba4ad0863a3582c
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-02-21 17:35:27 +0700

    add function buatRentang

---
+++ tasks/fgosandi/task6.js
@@ -0,0 +1,10 @@
+const buatRentang = (mulai, selesai) => {
+  const result = []
+  for (let i = mulai; i <= selesai; i++) {
+    result.push(i)
+  }
+  console.log(result)
+}
+
+buatRentang(1, 5)
+buatRentang(5, 10)
