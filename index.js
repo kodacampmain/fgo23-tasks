b79bc959a7eec1d039fcf7c9484df571034508f4
Sri Bintang Sangga Islami <xsis.sribint@xl.co.id>
2025-02-21 19:24:50 +0700

    Calling function in index.js

--- index.js
+++ index.js
@@ -4,48 +4,76 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul(){
+function tasksIrul() {
     // Program: Sum Number
     // Success
-    const resultSum = irul.sumNumber(10,50);
+    const resultSum = irul.sumNumber(10, 50);
     console.log(resultSum);
 
     // Failed
-    try{
-        const result = irul.sumNumber("10",30);
+    try {
+        const result = irul.sumNumber("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
     // Program: Multiply Number
     // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
+    const resultMultiply = irul.multiplyNumbers(10, 50);
     console.log(resultMultiply);
 
     // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
+    try {
+        const result = irul.multiplyNumbers("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
     // Program: Convert Celcius to Fahrenheit
     // Success
     const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
+    console.log(
+        `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+    );
 
     // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
+    try {
+        const result = irul.celciusToFahrenheit("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
 }
 tasksIrul();
 
 /**
- * Task "Person Name"
+ * Task "Imanul Aufa"
  */
 
 // import your tasks here
+
+const {
+    triangle,
+    countArray,
+    triangleReverse,
+    processNumber,
+    cetakTabel,
+    buatRentang,
+} = require("./tasks/fgoimanulaufa/index.js");
+
+triangle(8);
+console.log("\n");
+
+console.log(countArray([1, 2, 3, 4]));
+console.log("\n");
+
+triangleReverse(8);
+console.log("\n");
+
+console.log(processNumber([1, 2, 3, 4, 5, 5, 7]));
+console.log("\n");
+
+cetakTabel(2);
+console.log("\n");
+
+buatRentang(2, 5);
