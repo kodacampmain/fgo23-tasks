f59ed22e184258339caac9b9854276a0a91322fa
rrhmn <raissarahmani18@gmail.com>
2025-02-21 19:12:19 +0700

    fungsi segitiga

---
+++ tasks/fgoraissa/no1.js
@@ -0,0 +1,13 @@
+const triangle = (num) => {
+    if (typeof num !== "number" || num <= 0) return console.log("data should be a positive number");
+    for (let row=1; row<=num; row++) {
+        let string = "";
+        for (i=1; i<=row; i++) {
+            string += i;
+        } console.log(string);
+    } 
+};
+
+module.exports = {
+    triangle,
+};
