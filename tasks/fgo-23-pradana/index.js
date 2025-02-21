d73d54a63d0010258ee8ca010e56f6006fb80144
mrpradana2 <mrpradana2@gmail.com>
2025-02-21 18:04:33 +0700

    perubahan nama function karena conflic

--- tasks/fgo-23-pradana/index.js
+++ tasks/fgo-23-pradana/index.js
@@ -1,7 +1,7 @@
 import { triangle } from "./src/soal-no-1.js";
 import { countArray } from "./src/soal-no-2.js";
 import { triangle3 } from "./src/soal-no-3.js";
-import { countArray } from "./src/soal-no-4.js";
+import { countArray4 } from "./src/soal-no-4.js";
 import { cetakTabel } from "./src/soal-no-5.js";
 import { buatRentang } from "./src/soal-no-6.js";
 
@@ -16,8 +16,8 @@ countArray([2, 2, 2, 2, 2]);
 triangle3(5);
 
 // soal no-4
-countArray([1, 2, 3, 4, 5]);
-countArray([2, 4, 6, 8]);
+countArray4([1, 2, 3, 4, 5]);
+countArray4([2, 4, 6, 8]);
 
 // soal no-5
 cetakTabel(8);
--- tasks/fgo-23-pradana/src/soal-no-4.js
+++ tasks/fgo-23-pradana/src/soal-no-4.js
@@ -1,4 +1,4 @@
-export function countArray(inputArray) {
+export function countArray4(inputArray) {
   const oddNumbers = inputArray.filter((input) => input % 2 === 1);
   if (oddNumbers.length === 0) return console.log(1);
   return console.log(oddNumbers.reduce((a, b) => a * b));
