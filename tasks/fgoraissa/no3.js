e492065728810f161a559bb49e1b67dcbf181946
rrhmn <raissarahmani18@gmail.com>
2025-02-21 19:13:13 +0700

    fungsi segitiga

---
+++ tasks/fgoraissa/no3.js
@@ -0,0 +1,13 @@
+const segitiga = (num) => {
+    if (typeof num !== "number" || num <= 0) return console.log("data should be a positive number");
+    for (let row=1; row<=num; row++) {
+        let string = "";
+        for (i=row; i>0; i--) {
+            string += i;
+        } console.log(string);
+    } 
+};
+
+module.exports = {
+    segitiga,
+};
