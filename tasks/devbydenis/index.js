f8f3ad02114444054c65e6254f0d51e088ca3c3c
devbydenis <hello.denisrahmadi@gmail.com>
2025-02-21 18:18:19 +0700

    feat: index.js

---
+++ tasks/devbydenis/index.js
@@ -0,0 +1,15 @@
+const nomorSatu = require("./nomor-1.js");
+const nomorDua = require("./nomor-2.js");
+const nomorTiga = require("./nomor-3.js");
+const nomorEmpat = require("./nomor-4.js");
+const nomorLima = require("./nomor-5.js");
+const nomorEnam = require("./nomor-6.js");
+
+module.exports = {
+  nomorSatu,
+  nomorDua,
+  nomorTiga,
+  nomorEmpat,
+  nomorLima,
+  nomorEnam,
+};
