1797a9f42ebf9efa1abe9dc48b2f20e966cd616c
Rieza Eka Tomara <riezaekatomara@gmail.com>
2025-02-21 19:49:13 +0700

    indexnew1

--- index.js
+++ index.js
@@ -65,15 +65,12 @@ const {
 } = require("./tasks/fgorieza/index.js");
 
 function rieza() {
-  triangle("abc");
-  console.log("\n");
-  triangle(-1);
-  console.log("\n");
-  triangle(1);
-  console.log("\n");
-  triangle(2);
-  console.log("\n");
-  triangle(5);
+  triangleAs()
+  countArray()
+  triangleDes()
+  processNumber()
+  cetakTabel()
+  buatRentang()
 }
 
 rieza()
