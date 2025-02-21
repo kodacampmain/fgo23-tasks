8e3106a5ab4ce6876dc58ab6943a83ab9852b963
code with rizki <mrizki.nurrohman10@gmail.com>
2025-02-21 19:50:44 +0700

    add tiga.js

---
+++ tasks/fgorohman/tiga.js
@@ -0,0 +1,15 @@
+const triangle2 = (n) => {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = i; j >= 1; j--) {
+      result += ` ${j}`;
+    }
+    result += "\n";
+  }
+  return result;
+};
+
+const test = triangle2(5);
+console.log(test);
+
+module.exports = triangle2;
