a699b04516497f41220b6273eb9beab52f654fb6
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-02-21 17:44:43 +0700

    add index.js

---
+++ tasks/fgosandi/index.js
@@ -0,0 +1,15 @@
+import triangle from './task1'
+import countArray from './task2'
+import triangleReverse from './task3'
+import processNumber from './task4'
+import cetakTebal from './task5'
+import buatRentang from './task6'
+
+export {
+  triangle,
+  buatRentang,
+  cetakTebal,
+  countArray,
+  processNumber,
+  triangleReverse,
+}
