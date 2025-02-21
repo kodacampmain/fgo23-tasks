38def80f7a1d9d43b9d8e14ef2e6788af1fced91
yasirmaxstyle <yasirmu77@gmail.com>
2025-02-21 20:36:03 +0700

    fix: export

--- tasks/fgoyasir/index.js
+++ tasks/fgoyasir/index.js
@@ -5,21 +5,11 @@ const processNumber = require("./4_processNumber");
 const printTable = require("./5_printTable");
 const makeRange = require("./6_makeRange");
 
-function yasirCall() {
-    console.log(triangle(5));
-    console.log(countArray([1, 2, 3, 4, 5]));
-    console.log(triangleRev(5));
-    console.log(processNumber([1, 2, 3, 4, 5]));
-    printTable(3);
-    console.log(makeRange(1, 5));
-}
-
 module.exports = {
     triangle,
     countArray,
     triangleRev,
     processNumber,
     printTable,
-    makeRange,
-    yasirCall
+    makeRange
 };
