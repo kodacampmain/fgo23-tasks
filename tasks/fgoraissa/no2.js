75a309ad3b86204bcb67b9b67b434df9b6e02126
rrhmn <raissarahmani18@gmail.com>
2025-02-21 19:12:53 +0700

    fungsi countArray

---
+++ tasks/fgoraissa/no2.js
@@ -0,0 +1,12 @@
+const countArray = (arrNum) => {
+    if (!(arrNum instanceof Array)) return console.log("data should in array type");
+    for (const num of arrNum) {
+        if (typeof num !== "number") return console.log("data should be array of number"); 
+    }
+    const result = arrNum.reduce((num, length) => num * length, 1);
+    console.log(result);
+};
+
+module.exports = {
+    countArray,
+};
