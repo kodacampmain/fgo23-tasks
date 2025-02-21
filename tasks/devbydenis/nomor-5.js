4c60da9c25525c4456c417be5f3814381c8d46df
devbydenis <hello.denisrahmadi@gmail.com>
2025-02-21 19:22:46 +0700

    add: nomor-5.js to print table base on input

--- tasks/devbydenis/nomor-5.js
+++ tasks/devbydenis/nomor-5.js
@@ -2,16 +2,17 @@ function cetakTabel(n) {
     if (typeof n !== "number") return "input harus number";
     if (n < 1) return "input harus bilangan positif";
 
-    let result = 0;
-    for (let i = 0; i < 10; i++) {
-        result = n * (i + 1);
-        console.log(`${n} x ${i + 1} = ${result}`);
+    let result = "";
+    for (let i = 1; i <= 10; i++) {
+        // result = `${n} x ${i + 1} = ${result}`;
+        result += n + " x " + (i + 1) + " = " + (n * i) + "\n";
     }
-  
+
+    return result;
 }
 
 // console.log(cetakTabel("abc"))
 // console.log(cetakTabel(-1))
-// console.log(cetakTabel(3))
+// console.log(cetakTabel(3));
 
 module.exports = cetakTabel;
