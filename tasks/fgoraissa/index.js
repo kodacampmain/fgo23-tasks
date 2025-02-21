e91705716d3a713c80e4be6b283066f33d37d6cd
rrhmn <raissarahmani18@gmail.com>
2025-02-21 19:15:04 +0700

    all minitask

---
+++ tasks/fgoraissa/index.js
@@ -0,0 +1,15 @@
+const triangle = require ("./no1");
+const countArray = require ("./no2");
+const segitiga = require ("./no3");
+const processNumbers = require ("./no4");
+const cetakTabel = require ("./no5");
+const buatRentang = require ("./no6");
+
+module.exports = {
+    triangle,
+    countArray,
+    segitiga,
+    processNumbers,
+    cetakTabel,
+    buatRentang,
+};
