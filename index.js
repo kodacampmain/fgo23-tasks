87c9d72739a8bc7eb2f787fb11f03a4e019514fc
code with rizki <mrizki.nurrohman10@gmail.com>
2025-02-21 18:57:53 +0700

    add index.js utama

--- index.js
+++ index.js
@@ -4,48 +4,69 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul(){
+function tasksIrul() {
     // Program: Sum Number
     // Success
-    const resultSum = irul.sumNumber(10,50);
+    const resultSum = irul.sumNumber(10, 50);
     console.log(resultSum);
 
     // Failed
-    try{
-        const result = irul.sumNumber("10",30);
+    try {
+        const result = irul.sumNumber("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
     // Program: Multiply Number
     // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
+    const resultMultiply = irul.multiplyNumbers(10, 50);
     console.log(resultMultiply);
 
     // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
+    try {
+        const result = irul.multiplyNumbers("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
     // Program: Convert Celcius to Fahrenheit
     // Success
     const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
+    console.log(
+        `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+    );
 
     // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
+    try {
+        const result = irul.celciusToFahrenheit("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
 }
 tasksIrul();
 
 /**
- * Task "Person Name"
+ * Task Rohman
  */
 
 // import your tasks here
+
+const {
+    triangle,
+    hasilPerkalian,
+    triangle2,
+    //   number,
+    angka,
+    buatRentang,
+} = require("./tasks/fgorohman");
+
+console.log(triangle(5));
+console.log("\n");
+hasilPerkalian([1, 2, 3, 4, 5]);
+hasilPerkalian([2, 2, 2, 2, 2]);
+console.log("\n");
+triangle2(5);
+// number([1, 2, 3, 4, 5]);
+angka(9);
+buatRentang(1, 10);
