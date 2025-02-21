dc086b2b7e1675cf07f24c962f64e30450cf808d
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-21 18:06:42 +0700

    feat: add function proccessNumber

---
+++ tasks/mhakimsaputra/number4.js
@@ -0,0 +1,20 @@
+const proccessNumber = (nums) => {
+  if (!(nums instanceof Array)) {
+    return console.log("Parameter harus bertipe data array");
+  }
+
+  let result = 1;
+  for (element of nums) {
+    if (element % 2 !== 0) {
+      result *= element;
+    } else {
+      continue;
+    }
+  }
+  return console.log(result);
+};
+
+// proccessNumber([1, 2, 3, 4, 5]);
+// proccessNumber([2, 4, 6, 8]);
+
+module.exports = proccessNumber;
