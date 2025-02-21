e2a69e503bfff1765c9b515e7d8b27c3af277553
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-21 18:08:12 +0700

    feat:index.js

---
+++ tasks/mhakimsaputra/index.js
@@ -0,0 +1,15 @@
+const triangle = require("./number1");
+const countArray = require("./number2");
+const triangle3 = require("./number3");
+const proccessNumber = require("./number4");
+const cetakTabel = require("./number5");
+const buatRentang = require("./number6");
+
+module.exports = {
+  triangle,
+  countArray,
+  triangle3,
+  proccessNumber,
+  cetakTabel,
+  buatRentang,
+};
