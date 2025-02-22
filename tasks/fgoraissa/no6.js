a917ac85a11323a5e29c5f0a4dd21a0223b04eda
rrhmn <raissarahmani18@gmail.com>
2025-02-22 20:21:41 +0700

    task 6: fungsi cetak rentang nomor

--- tasks/fgoraissa/no6.js
+++ tasks/fgoraissa/no6.js
@@ -1,8 +1,11 @@
 const buatRentang = (start,end) => {
     if (typeof start !== "number" || typeof end !== "number") return console.log("data should be number");
+    let range = [];
     for (let i=start; i<=end; i++) {
-        console.log(i);
-    } 
+        range.push(i);
+    }
+    console.log(range.join(","));
+    return;
 };
 
 module.exports = {
