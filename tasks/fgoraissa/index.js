2c56f852722fc4c6c67ba5ba26ce0175cdac97ed
rrhmn <raissarahmani18@gmail.com>
2025-02-22 20:22:11 +0700

    compile task

--- tasks/fgoraissa/index.js
+++ tasks/fgoraissa/index.js
@@ -1,15 +1,19 @@
-const triangle = require ("./no1");
-const countArray = require ("./no2");
-const segitiga = require ("./no3");
-const processNumbers = require ("./no4");
-const cetakTabel = require ("./no5");
-const buatRentang = require ("./no6");
+const {triangle} = require ("./no1");
+const {countArray} = require ("./no2");
+const {segitiga} = require ("./no3");
+const {processNumbers} = require ("./no4");
+const {cetakTabel} = require ("./no5");
+const {buatRentang} = require ("./no6");
+
+const compileMinitask = (num,arrNum,start,end) => {
+    triangle(num);
+    countArray(arrNum);
+    segitiga(num);
+    processNumbers(arrNum);
+    cetakTabel(num);
+    buatRentang(start, end);
+};
 
 module.exports = {
-    triangle,
-    countArray,
-    segitiga,
-    processNumbers,
-    cetakTabel,
-    buatRentang,
+    compileMinitask,
 };
