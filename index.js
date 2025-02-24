8930f107d5b5c874968ed7e3eaf34c0fe429cd68
code with rizki <mrizki.nurrohman10@gmail.com>
2025-02-24 10:07:11 +0700

    perbaikan2 spasi file index.js kak irul

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ const irul = require("./tasks/irul");
 function tasksIrul(){
     // Program: Sum Number
     // Success
-    const resultSum = irul.sumNumber(10, 50);
+    const resultSum = irul.sumNumber(10,50);
     console.log(resultSum);
 
     // Failed
     try{
-        const result = irul.sumNumber("10", 30);
+        const result = irul.sumNumber("10",30);
         console.log(result);
     }catch (error){
         console.log(error.message);
     }
     // Program: Multiply Number
     // Success
-    const resultMultiply = irul.multiplyNumbers(10, 50);
+    const resultMultiply = irul.multiplyNumbers(10,50);
     console.log(resultMultiply);
 
     // Failed
     try{
-        const result = irul.multiplyNumbers("10", 30);
+        const result = irul.multiplyNumbers("10",30);
         console.log(result);
     }catch (error){
         console.log(error.message);
@@ -32,13 +32,11 @@ function tasksIrul(){
     // Program: Convert Celcius to Fahrenheit
     // Success
     const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(
-        `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
-    );
+    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
 
     // Failed
     try{
-        const result = irul.celciusToFahrenheit("10", 30);
+        const result = irul.celciusToFahrenheit("10",30);
         console.log(result);
     }catch (error){
         console.log(error.message);
