7e70ed92051bd2c71ba3a1b63876de9013e1d0fd
Mamad <moezaen25@gmail.com>
2025-02-24 09:43:54 +0700

    Revisi Solusi Minitask

--- index.js
+++ index.js
@@ -4,43 +4,45 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul(){
-    // Program: Sum Number
-    // Success
-    const resultSum = irul.sumNumber(10,50);
-    console.log(resultSum);
+function tasksIrul() {
+  // Program: Sum Number
+  // Success
+  const resultSum = irul.sumNumber(10, 50);
+  console.log(resultSum);
 
-    // Failed
-    try{
-        const result = irul.sumNumber("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Multiply Number
-    // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
-    console.log(resultMultiply);
+  // Failed
+  try {
+    const result = irul.sumNumber("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Multiply Number
+  // Success
+  const resultMultiply = irul.multiplyNumbers(10, 50);
+  console.log(resultMultiply);
 
-    // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Convert Celcius to Fahrenheit
-    // Success
-    const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
+  // Failed
+  try {
+    const result = irul.multiplyNumbers("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Convert Celcius to Fahrenheit
+  // Success
+  const temperatureResult = irul.celciusToFahrenheit(10);
+  console.log(
+    `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+  );
 
-    // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
+  // Failed
+  try {
+    const result = irul.celciusToFahrenheit("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 tasksIrul();
 
@@ -49,29 +51,7 @@ tasksIrul();
  */
 
 // import your tasks here
-const { createTriangle, countArray, triangle1, processNumbers, cetakTabel, buatRentang } = require("./tasks/fgomamad/index");
 
-createTriangle("abc");
-createTriangle(-1);
-createTriangle(1);
-createTriangle(2);
-createTriangle(5);
+const { outputTask } = require("./tasks/fgomamad/index.js");
 
-countArray([1, 2, 3, 4, 5]);
-countArray([2, 2, 2, 2, 2]);
-
-triangle1("abc");
-triangle1(-1);
-triangle1(1);
-triangle1(2);
-triangle1(5);
-
-processNumbers([1, 2, 3, 4, 5]);
-processNumbers([2, 4, 6, 8]);
-
-cetakTabel("abc");
-cetakTabel(-1);
-cetakTabel(3);
-
-buatRentang(1, 5);
-buatRentang(5, 10);
+outputTask();
--- tasks/fgomamad/index.js
+++ tasks/fgomamad/index.js
@@ -8,4 +8,32 @@ const buatRentang = require("./soal6");
 
 createTriangle(3);
 
-module.exports = { createTriangle, countArray, triangle1, processNumbers, cetakTabel, buatRentang };
+function outputTask(){
+    createTriangle("abc");
+    createTriangle(-1);
+    createTriangle(1);
+    createTriangle(2);
+    createTriangle(5);
+    
+    countArray([1, 2, 3, 4, 5]);
+    countArray([2, 2, 2, 2, 2]);
+    
+    triangle1("abc");
+    triangle1(-1);
+    triangle1(1);
+    triangle1(2);
+    triangle1(5);
+    
+    processNumbers([1, 2, 3, 4, 5]);
+    processNumbers([2, 4, 6, 8]);
+    
+    cetakTabel("abc");
+    cetakTabel(-1);
+    cetakTabel(3);
+    
+    buatRentang(1, 5);
+    buatRentang(5, 10);
+    
+}
+
+module.exports = { outputTask }
