730035d73e52ee14bb96aefc27fbc6d2241cd155
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-02-24 09:39:20 +0700

    fix : Repair by removing destructing because it will conflict with your friend's work

--- index.js
+++ index.js
@@ -4,43 +4,45 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul(){
-    // Program: Sum Number
-    // Success
-    const resultSum = irul.sumNumber(10,50);
-    console.log(resultSum);
+function tasksIrul() {
+  // Program: Sum Number
+  // Success
+  const resultSum = irul.sumNumber(10, 50);
+  console.log(resultSum);
 
-    // Failed
-    try{
-        const result = irul.sumNumber("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Multiply Number
-    // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
-    console.log(resultMultiply);
+  // Failed
+  try {
+    const result = irul.sumNumber("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Multiply Number
+  // Success
+  const resultMultiply = irul.multiplyNumbers(10, 50);
+  console.log(resultMultiply);
 
-    // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Convert Celcius to Fahrenheit
-    // Success
-    const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
+  // Failed
+  try {
+    const result = irul.multiplyNumbers("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Convert Celcius to Fahrenheit
+  // Success
+  const temperatureResult = irul.celciusToFahrenheit(10);
+  console.log(
+    `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+  );
 
-    // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
+  // Failed
+  try {
+    const result = irul.celciusToFahrenheit("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 tasksIrul();
 
@@ -52,23 +54,25 @@ tasksIrul();
 /**
  * Task "Person Name"
  */
-const {
-  triangle,
-  countArray,
-  prinstSegitiga,
-  countArrayOdd,
-  cetakTabel,
-  buatRentang,
-} = require("./tasks/habib/index.js");
+const tasksHabib = require("./tasks/habib/index.js");
 
-triangle(5);
-countArray([1, 2, 3, 4, 5]);
-countArray([2, 2, 2, 2, 2]);
-prinstSegitiga(5);
-countArrayOdd([1, 2, 3, 4, 5]);
-countArrayOdd([2, 4, 6, 8]);
-cetakTabel(3);
-buatRentang(1, 5);
-buatRentang(5, 10);
+// Soal no 1
+tasksHabib.triangle(5);
+
+// Soal no 2
+tasksHabib.countArray([1, 2, 3, 4, 5]);
+tasksHabib.countArray([2, 2, 2, 2, 2]);
+
+// Soal no 3
+tasksHabib.prinstSegitiga(5);
+
+// Soal no 4
+tasksHabib.countArrayOdd([1, 2, 3, 4, 5]);
+tasksHabib.countArrayOdd([2, 4, 6, 8]);
+// Soal no 5
+tasksHabib.cetakTabel(3);
+// Soal no 6
+tasksHabib.buatRentang(1, 5);
+tasksHabib.buatRentang(5, 10);
 
 // import your tasks here
