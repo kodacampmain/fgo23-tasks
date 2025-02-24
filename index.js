06fcf71839d367ccacc0e2fe263aaab5faadf5d6
mrpradana2 <mrpradana2@gmail.com>
2025-02-24 09:38:53 +0700

    fix: improvement are distructuring so as not to conflict with other friend

--- index.js
+++ index.js
@@ -4,43 +4,45 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul(){
-    // Program: Sum Number
-    // Success
-    const resultSum = irul.sumNumber(10,50);
-    console.log(resultSum);
+function tasksIrul() {
+  // Program: Sum Number
+  // Success
+  const resultSum = irul.sumNumber(10, 50);
+  console.log(resultSum);
 
-    // Failed
-    try{
-        const result = irul.sumNumber("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Multiply Number
-    // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
-    console.log(resultMultiply);
+  // Failed
+  try {
+    const result = irul.sumNumber("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Multiply Number
+  // Success
+  const resultMultiply = irul.multiplyNumbers(10, 50);
+  console.log(resultMultiply);
 
-    // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Convert Celcius to Fahrenheit
-    // Success
-    const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
+  // Failed
+  try {
+    const result = irul.multiplyNumbers("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Program: Convert Celcius to Fahrenheit
+  // Success
+  const temperatureResult = irul.celciusToFahrenheit(10);
+  console.log(
+    `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+  );
 
-    // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
+  // Failed
+  try {
+    const result = irul.celciusToFahrenheit("10", 30);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 tasksIrul();
 
@@ -48,29 +50,27 @@ tasksIrul();
  * Task "Muhammad Rizki Pradana"
  */
 
-const { triangle, countArray, triangle3, countArray4, cetakTabel, buatRentang } = require('./tasks/fgo-23-pradana/index')
-
+const tasksPradana = require("./tasks/fgo-23-pradana/index");
 
 // soal no-1
-triangle(5);
+tasksPradana.triangle(5);
 
 // soal no-2
-countArray([1, 2, 3, 4, 5]);
-countArray([2, 2, 2, 2, 2]);
+tasksPradana.countArray([1, 2, 3, 4, 5]);
+tasksPradana.countArray([2, 2, 2, 2, 2]);
 
 // soal no-3
-triangle3(5);
+tasksPradana.triangle3(5);
 
 // soal no-4
-countArray4([1, 2, 3, 4, 5]);
-countArray4([2, 4, 6, 8]);
+tasksPradana.countArray4([1, 2, 3, 4, 5]);
+tasksPradana.countArray4([2, 4, 6, 8]);
 
 // soal no-5
-cetakTabel(8);
+tasksPradana.cetakTabel(8);
 
 // soal no-6
-buatRentang(1, 5);
-buatRentang(5, 10);
-
+tasksPradana.buatRentang(1, 5);
+tasksPradana.buatRentang(5, 10);
 
 // import your tasks here
